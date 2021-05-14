2918d0215c5d0197dbf2bcedbfd0689e807be8a1
Harshank <pathakharsh13@gmail.com>
2021-05-14 20:30:50 +0530

    Delete game_mode_on.sh

--- game_mode_on.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-pkexec echo 8 >/sys/devices/system/cpu/intel_pstate/min_perf_pct
-echo $?
-  if [ $? -eq 0 ]; then
-   notify-send 'Success' 'Game mode ON'
-else
-   notify-send 'Failed' 'Game mode failed to exec'
-fi
