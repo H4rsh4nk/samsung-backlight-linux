052f64926110991fa6610b0d05461813fa6bc145
Harshank <pathakharsh13@gmail.com>
2021-05-15 14:23:59 +0530

    Add files via upload

--- dec_baclit.sh
+++ dec_baclit.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
+stat=$(stat -c '%a' /sys/class/leds/samsung::kbd_backlight/brightness)
+if [ $stat -ne 777 ]; then
+   echo "toor" | sudo -S chmod ugo+rwx /sys/class/leds/samsung::kbd_backlight/brightness
+   if [ $? -eq 0 ]; then
+      notify-send 'Success' 'Keyboard backlight unlocked ｡◕‿◕｡'
+   else
+      notify-send 'Failed' 'Keyboard backlight still locked.
+   Please try again. 
 
-for file in $(cat /sys/class/leds/samsung::kbd_backlight/brightness)
-do
-echo "$((file-1))" > /sys/class/leds/samsung::kbd_backlight/brightness
-done
+   ʕ´• ᴥ•̥`ʔ'
+   fi
+fi
+
+brightness=$(cat /sys/class/leds/samsung::kbd_backlight/brightness)
+if [ $brightness -ge 1 ]; then
+    for file in $(cat /sys/class/leds/samsung::kbd_backlight/brightness)
+    do
+    echo "$((file-1))" > /sys/class/leds/samsung::kbd_backlight/brightness
+    done
+fi
