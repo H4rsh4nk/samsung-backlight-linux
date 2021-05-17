f3707b5d43a8ad84b63620e3cb9c3061734e63d2
Harshank <pathakharsh13@gmail.com>
2021-05-17 19:20:39 +0530

    Add files via upload

---
+++ one_key_bind.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+stat=$(stat -c '%a' /sys/class/leds/samsung::kbd_backlight/brightness)
+if [ $stat -ne 777 ]; then
+   echo "toor" | sudo -S chmod ugo+rwx /sys/class/leds/samsung::kbd_backlight/brightness
+   if [ $? -eq 0 ]; then
+      notify-send 'Success' 'Keyboard backlight unlocked ｡◕‿◕｡'
+   else
+      notify-send 'Failed' 'Keyboard backlight still locked.
+   Please try again. 
+
+   ʕ´• ᴥ•̥`ʔ'
+   fi
+fi
+
+brightness=$(cat /sys/class/leds/samsung::kbd_backlight/brightness)
+if [ $brightness -eq 3 ]; then
+   echo 0 > /sys/class/leds/samsung::kbd_backlight/brightness
+   notify-send 'Brightness' 'off'
+else
+   if [ $brightness -eq 0 ]; then
+      notify-send 'Brightness' 'Low'
+   elif [ $brightness -eq 1 ]; then
+      notify-send 'Brightness' 'Medium'
+   else
+      notify-send 'Brightness' 'High'
+   fi
+  
+   for file in $(cat /sys/class/leds/samsung::kbd_backlight/brightness)
+   do
+      echo "$((file+1))" > /sys/class/leds/samsung::kbd_backlight/brightness
+   done
+fi
